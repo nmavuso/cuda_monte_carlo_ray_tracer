4bde7b82ab5b5926c4e8a115118745d99b111e99
nmavuso <nmavuso@seas.upenn.edu>
2025-02-05 01:58:33 -0500

    failing

---
+++ build/CMakeFiles/path_tracer.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/path_tracer.dir/cmake_device_link.o"
+  "CMakeFiles/path_tracer.dir/src/bvh.cu.o"
+  "CMakeFiles/path_tracer.dir/src/camera.cu.o"
+  "CMakeFiles/path_tracer.dir/src/hitable.cu.o"
+  "CMakeFiles/path_tracer.dir/src/hitable_list.cu.o"
+  "CMakeFiles/path_tracer.dir/src/main.cu.o"
+  "CMakeFiles/path_tracer.dir/src/material.cu.o"
+  "CMakeFiles/path_tracer.dir/src/sphere.cu.o"
+  "path_tracer"
+  "path_tracer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/path_tracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
